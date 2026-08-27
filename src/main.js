4394142ff7fbc0d1ca394b85c20f46cbd7aec501
Niraj Gambhava <niraj.gambhava@gmail.com>
2026-08-27 16:44:41 +0530

    chore: remove remaining project files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { createServer } from 'node:http';
+const { createServer } = require("node:http");
 
 const port = Number(process.env.PORT ?? 8000);
 
 createServer((_request, response) => {
   response.statusCode = 200;
-  response.setHeader('Cache-Control', 'no-store');
+  response.setHeader("Cache-Control", "no-store");
   response.end();
 }).listen(port);
